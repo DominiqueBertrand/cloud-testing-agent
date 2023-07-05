0569efdcc3e4256b571d97cc8429b483bdaf1a8f
Dominique BERTRAND <dominique.bertrand@coopengo.com>
2023-07-05 12:10:27 +0200

    add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from '@src/app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 // import serviceConfig from '@src/config/service.config';
 
 async function bootstrap() {
@@ -11,7 +12,18 @@ async function bootstrap() {
   app.enableCors();
   const configService: ConfigService = app.get<ConfigService>(ConfigService);
   const port = configService.get('service.port');
-
+  // <-- SWAGGER
+  const config = new DocumentBuilder()
+    .setTitle('Coog Cloud Agent')
+    .setDescription(
+      'Coog Cloud Agent is a specialized tool suite for testing and monitoring the entire application-delivery chain within the Coopengo Cloud Application Platform.',
+    )
+    .setVersion('1.0')
+    .addTag('Postman')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // SWAGER -->
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
--- src/postman/postman.controller.ts
+++ src/postman/postman.controller.ts
@@ -1,53 +1,61 @@
 import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PostmanService } from './postman.service';
-import { tests } from './postman.model';
+import { tests, PostmanModel } from './postman.model';
 
+@Controller('postman')
+@ApiTags('Postman')
 @Controller('postman')
 export class PostmanController {
   constructor(private postmanService: PostmanService) {}
 
   // Collections
   @Post('collections_list')
-  // create a list of all collections
+  @ApiOperation({ summary: 'Generate a list of all collections' })
   addCollectionsList(): object {
     return this.postmanService.insertCollectionsList();
   }
 
   @Get('collections_list')
-  // get a list of all collections
+  @ApiOperation({ summary: 'Get a list of all collections' })
   getCollectionsList() {
     return this.postmanService.getCollectionsList();
   }
 
   // Environnements
   @Post('environnement_list')
-  // create a list of all environnements
+  @ApiOperation({ summary: 'Generate a list of all environnements' })
   addEnvironnementList(): object {
     return this.postmanService.insertEnvironnementList();
   }
 
   @Get('environnement_list')
-  // get a list of all environnements
+  @ApiOperation({ summary: 'Get a list of all environnements' })
   getEnvironnementList() {
     return this.postmanService.getEnvironnementList();
   }
 
   // Tests
   @Post('create_newman')
-  // Create newman test
+  @ApiOperation({ summary: 'Create newman test' })
   addTest(@Body('id') id: string, @Body('gobal_env_id') env_id: string, @Body('tests') test: tests): object {
     return this.postmanService.insertTest(id, env_id, test);
   }
 
   @Get('test')
-  // get a list of all tests
+  @ApiOperation({ summary: 'Get a list of all tests' })
   getAllTest() {
     return this.postmanService.getTests();
   }
 
   //TODO
   @Get('test/:id')
-  // get test by ID
+  @ApiOperation({ summary: 'Get a test by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'The found test',
+    type: PostmanModel,
+  })
   getTest(@Param('id') testID: string) {
     return this.postmanService.getSingleTest(testID);
   }
