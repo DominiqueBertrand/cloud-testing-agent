bf4ff673f81b65808af6344bf81f6467a746763f
Dominique BERTRAND <dominique.bertrand@coopengo.com>
2023-07-25 18:26:42 +0200

    update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 
 import serviceConfig from '@src/config/service.config';
 // Modules
-import { PostmanModule } from './modules/postman/postman.module';
 import { OrmModule } from './modules/orm/orm.module';
 import { TaskModule } from './modules/task/task.module';
 import { PmCollectionModule } from './modules/pmCollection/pmCollection.module';
@@ -15,7 +14,6 @@ import { UsersModule } from './modules/users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ ignoreEnvFile: true, load: [serviceConfig] }),
-    PostmanModule,
     OrmModule,
     TaskModule,
     PmCollectionModule,
