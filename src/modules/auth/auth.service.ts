af172e353c2279b6934e7184b3d23eb75fdbcd97
Dominique BERTRAND <dominique.bertrand@coopengo.com>
2023-07-28 17:33:04 +0200

    improvement of the authentication modules

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,22 +1,130 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UsersService } from '../users/users.service';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { EntityManager, EntityRepository } from '@mikro-orm/core';
+import { ConfigService } from '@nestjs/config';
+import { SignOptions } from 'jsonwebtoken';
+import { v4 } from 'uuid';
+
+import { UserService } from '../users/user.service';
+import { RefreshSession, User } from '@src/entities';
+import { LoginDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private usersService: UsersService,
+    @InjectRepository(RefreshSession) private readonly sessionRepository: EntityRepository<RefreshSession>,
+    private userService: UserService,
     private jwtService: JwtService,
+    private configService: ConfigService,
+
+    private readonly em: EntityManager,
   ) {}
 
-  async signIn(username, pass) {
-    const user = await this.usersService.findOne(username);
-    if (user?.password !== pass) {
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user: User = await this.userService.getUserByUsername(username);
+
+    if (user && user.password === pass) {
+      const dynamicKey = 'password';
+      const { [dynamicKey]: _, ...rest } = user;
+      return rest;
+    }
+
+    return null;
+  }
+
+  async login(login: LoginDto) {
+    const user: User = await this.userService.getUserByUsername(login.username);
+    const accessToken = await this.generateAccessToken({ id: user.id, username: user.username });
+    const refreshToken = await this.generateRefreshToken({ id: user.id });
+
+    await this.createRefreshSession(user, refreshToken);
+
+    const dynamicKey = 'password';
+    const { [dynamicKey]: _, ...rest } = user;
+
+    return {
+      user: rest,
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async createRefreshSession(user: User, token: string) {
+    const decodeToken: any = this.jwtService.decode(token);
+
+    const refreshToken = token;
+    const expiresIn = decodeToken.exp;
+    const createdAt: Date = decodeToken.iat;
+
+    const session = new RefreshSession(user, refreshToken, expiresIn, createdAt);
+
+    this.em.persist(session);
+    await this.em.flush();
+
+    return session;
+  }
+
+  async generateAccessToken(payload: { id: string; username: string | undefined }): Promise<string> {
+    const expiresIn = this.configService.get<string>('service.jwt.jwtAccessExpiresIn');
+    const subject = String(payload.id);
+    const opts: SignOptions = {
+      expiresIn,
+      subject,
+    };
+
+    return this.jwtService.signAsync(
+      {
+        username: payload.username,
+        sid: v4(), // token uniqueness
+      },
+      opts,
+    );
+  }
+
+  async generateRefreshToken(payload: { id: string }): Promise<string> {
+    const opts: SignOptions = {
+      expiresIn: this.configService.get<string>('service.jwt.jwtRefreshExpiresIn'),
+      subject: String(payload.id),
+    };
+
+    return this.jwtService.signAsync(
+      {
+        sid: v4(), // token uniqueness
+      },
+      opts,
+    );
+  }
+
+  async refreshToken(token: string) {
+    let decodeToken: any;
+    try {
+      decodeToken = this.jwtService.verify(token);
+    } catch (err) {
       throw new UnauthorizedException();
     }
-    const payload = { username: user.username, sub: user.userId };
+
+    const user: User = await this.userService.getUserById(decodeToken.sub);
+
+    const session: RefreshSession | null = await this.sessionRepository.findOne({ refreshToken: token });
+    if (!session) throw new UnauthorizedException();
+
+    const accessToken = await this.generateAccessToken({ id: user.id, username: user.username });
+    const refreshToken = await this.generateRefreshToken({ id: user.id });
+
+    const decodeNewToken: any = this.jwtService.decode(refreshToken);
+
+    session.createdAt = decodeNewToken.iat;
+    session.expiresIn = decodeNewToken.exp;
+    session.refreshToken = refreshToken;
+
+    this.em.persist(session);
+    await this.em.flush();
+
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      user,
+      accessToken,
+      refreshToken,
     };
   }
 }
--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@src/entities';
 import { IsString } from 'class-validator';
 
 export class LoginDto {
@@ -13,4 +14,24 @@ export class LoginDto {
     required: true,
   })
   readonly password!: string;
+}
+
+export class LoginResponseDto {
+  @IsString()
+  @ApiProperty({
+    required: true,
+  })
+  readonly user!: User;
+
+  @IsString()
+  @ApiProperty({
+    required: true,
+  })
+  readonly accessToken!: string;
+
+  @IsString()
+  @ApiProperty({
+    required: true,
+  })
+  readonlyrefreshToken!: string;
 }
