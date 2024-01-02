a4a5702ba6ea63c0232dfe096d45a213f0320207
Dominique BERTRAND <dominique.bertrand@coopengo.com>
2024-01-02 16:44:05 +0100

    chore(global): update dependencies
    update dependencies && docker documentation

--- src/modules/task/task.utils.ts
+++ src/modules/task/task.utils.ts
@@ -26,30 +26,30 @@ export const sanitizeTask = (task: Task): ITask => {
 export type NullToUndefined<T> = T extends null
   ? undefined
   : T extends Primitive | Date | RegExp
-  ? T
-  : T extends Array<infer U>
-  ? Array<NullToUndefined<U>>
-  : T extends Map<infer K, infer V>
-  ? Map<K, NullToUndefined<V>>
-  : T extends Set<infer U>
-  ? Set<NullToUndefined<U>>
-  : T extends object
-  ? { [K in keyof T]: NullToUndefined<T[K]> }
-  : unknown;
+    ? T
+    : T extends Array<infer U>
+      ? Array<NullToUndefined<U>>
+      : T extends Map<infer K, infer V>
+        ? Map<K, NullToUndefined<V>>
+        : T extends Set<infer U>
+          ? Set<NullToUndefined<U>>
+          : T extends object
+            ? { [K in keyof T]: NullToUndefined<T[K]> }
+            : unknown;
 
 export type UndefinedToNull<T> = T extends undefined
   ? null
   : T extends Primitive | Date | RegExp
-  ? T
-  : T extends Array<infer U>
-  ? Array<UndefinedToNull<U>>
-  : T extends Map<infer K, infer V>
-  ? Map<K, UndefinedToNull<V>>
-  : T extends Set<infer U>
-  ? Set<NullToUndefined<U>>
-  : T extends object
-  ? { [K in keyof T]: UndefinedToNull<T[K]> }
-  : unknown;
+    ? T
+    : T extends Array<infer U>
+      ? Array<UndefinedToNull<U>>
+      : T extends Map<infer K, infer V>
+        ? Map<K, UndefinedToNull<V>>
+        : T extends Set<infer U>
+          ? Set<NullToUndefined<U>>
+          : T extends object
+            ? { [K in keyof T]: UndefinedToNull<T[K]> }
+            : unknown;
 
 function _nullToUndefined<T>(obj: T): NullToUndefined<T> {
   if (obj === null) {
