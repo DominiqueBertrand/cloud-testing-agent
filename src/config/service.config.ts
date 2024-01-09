be2c524c145c2242b284a3075106b1ffabc23ddc
Dominique BERTRAND <dominique.bertrand@coopengo.com>
2024-01-09 16:41:04 +0100

    chore(global): pm2

--- src/config/service.config.ts
+++ src/config/service.config.ts
@@ -5,6 +5,9 @@ export const jsonParserLimit: string = process.env.JSON_PARSER_LIMIT ? process.e
 export const jwtSecret: string = process.env.JWT_SECRET ? process.env.JWT_SECRET : 'secret';
 export const jwtAccessExpiresIn: string = process.env.JWT_ACCESS_EXPIRESIN ? process.env.JWT_ACCESS_EXPIRESIN : '15m';
 export const jwtRefreshExpiresIn: string = process.env.JWT_REFRESH_EXPIRESIN ? process.env.JWT_REFRESH_EXPIRESIN : '1h';
+export const dbName: string = process.env.DB_NAME ? process.env.DB_NAME : 'coog-cloud-agent.db.sqlite';
+export const dbPath: string = process.env.DB_PATH ? process.env.DB_PATH : dbName;
+export const dbPort: number = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3307;
 
 export default registerAs('service', () => ({
   port,
@@ -14,4 +17,7 @@ export default registerAs('service', () => ({
     jwtAccessExpiresIn,
     jwtRefreshExpiresIn,
   },
+  dbName,
+  dbPath,
+  dbPort,
 }));
